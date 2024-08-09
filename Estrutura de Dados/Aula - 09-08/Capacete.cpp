1b4e74fff744d0d35d786ec3405a52e30be2e455
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2024-08-09 19:55:28 -0300

    Update Capacete.cpp

---
+++ Estrutura de Dados/Aula - 09-08/Capacete.cpp
@@ -0,0 +1,16 @@
+// Seção de Implementação
+#include "Capacete.h"
+// importa as classes de Capacete.h
+#include <iostream>
+#include <string>
+using namespace std;
+
+// evitar a repetição da definição do codigo toda vez que der includ
+#ifndef CAPACETE_H
+#define CAPACETE_H
+
+void Cap::pressleft(){
+    cout << leftSlot << endl.
+}
+
+#endif

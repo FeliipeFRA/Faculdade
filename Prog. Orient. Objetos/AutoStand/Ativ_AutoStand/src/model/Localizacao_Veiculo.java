8477129c48882d35e9c088c91aba88db8e84c63c
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2025-02-24 21:26:28 -0300

    organization

--- Prog. Orient. Objetos/AutoStand/Ativ_AutoStand/src/model/Localizacao_Veiculo.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Localizacao_Veiculo {
-	
-	private char tipo;
-	private String endereco;
-	private short CEP;
-	private short num_endereco;
-}
--- Prog. Orient. Objetos/AutoStand/Ativ_AutoStand/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Ativ_AutoStand {
-}
